0beb76cb8cf7d093de23b9f4294e5f67040017c3
MryImani <maryam_imani22@yahoo.com>
2023-06-26 18:38:27 +0330

    add search event

---
+++ src/components/event/event-search.js
@@ -0,0 +1,45 @@
+import Button from "../ui/button";
+
+export default function EventSearch(props) {
+    return (
+      <form className="w-full md:w-6/12 max-w-2xl my-20 mx-auto bg-white  p-4 shadow-md rounded-md">
+        <div className="flex  justify-between items-center">
+          <div className="w-1/3">
+            <label className="font-bold mx-3" htmlFor="year">
+              Year
+            </label>
+            <select
+              className=" w-1/2 p-1 border-2 border-black rounded-md"
+              id="year"
+            >
+              <option value="2021">2021</option>
+              <option value="2022">2022</option>
+            </select>
+          </div>
+          <div className="w-1/3">
+            <label className="font-bold mx-3" htmlFor="month">
+              Month
+            </label>
+            <select
+              className=" w-1/2 p-1 border-2 border-black rounded-md"
+              id="month"
+            >
+              <option value="1">January</option>
+              <option value="2">February</option>
+              <option value="3">March</option>
+              <option value="4">April</option>
+              <option value="5">May</option>
+              <option value="6">June</option>
+              <option value="7">July</option>
+              <option value="8">August</option>
+              <option value="9">Septemer</option>
+              <option value="10">October</option>
+              <option value="11">November</option>
+              <option value="12">December</option>
+            </select>
+          </div>
+          <Button>Find Events</Button>
+        </div>
+      </form>
+    );
+}
--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,10 +1,12 @@
 import EventList from "@/components/event/event-list";
 import { getAllEvents } from "../../../dummy-data";
+import EventSearch from "@/components/event/event-search";
 
 export default function AllEvents() {
   const allEvents = getAllEvents();
   return (
     <div className="">
+      <EventSearch/>
       <EventList events={allEvents} />
     </div>
   );
