5d6d2caf0def0361f6987e80b7c229b7a73fc40f
MryImani <maryam_imani22@yahoo.com>
2023-06-26 14:14:25 +0330

    add button to explore link and icons

--- src/components/event/event-item.js
+++ src/components/event/event-item.js
@@ -1,4 +1,5 @@
-import Link from "next/link";
+import { ImCalendar, ImArrowRight, ImLocation } from "react-icons/im";
+import Button from "../ui/button";
 export default function EventItem(props) {
   const { id, title, location, date, image } = props;
 
@@ -8,24 +9,32 @@ export default function EventItem(props) {
     day: "numeric",
   });
 
-
-  const addressFormatted = location.replace(',','\n');
+  const addressFormatted = location.replace(",", "\n");
   const exploreLink = `/events/${id}`;
   return (
     <li className="flex flex-wrap md:flex-nowrap shadow-md rounded-lg overflow-hidden bg-white m-4 gap-4">
-      <img className=" h-48 w-full" src={"/" + image} alt={title} />
-      <div className="flex flex-col w-full px-4 text-center">
+      <img className=" h-52 w-full" src={"/" + image} alt={title} />
+      <div className="flex flex-col w-full px-4">
         <div className="">
           <h2 className="font-bold text-xl my-2 mx-0">{title}</h2>
-          <h3 className="font-bold text-gray-400">
-            {dateFormatted}
+          <h3 className="flex items-center  gap-2 font-bold text-gray-400">
+            <ImCalendar />
+            <span>{dateFormatted}</span>
           </h3>
-          <p className="my-2 mx-0  text-gray-400 whitespace-pre">
-            {addressFormatted}
+          <p className="flex items-center gap-2 my-2 mx-0  text-gray-400 whitespace-pre">
+            <ImLocation />
+            <span> {addressFormatted} </span>
           </p>
         </div>
-        <div>
-          <Link href={exploreLink}>Explore Event</Link>
+        <div className="flex justify-center my-4 md:justify-end ">
+          <Button link={exploreLink}>
+            <span className="flex items-center">
+              <span>Explore Event</span>
+              <span className="mx-1">
+                <ImArrowRight />
+              </span>
+            </span>
+          </Button>
         </div>
       </div>
     </li>
